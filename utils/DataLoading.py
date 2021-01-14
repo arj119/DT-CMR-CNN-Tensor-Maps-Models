a59fb642e24167246e38fd319eb5bde17d4be0fb
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2021-01-14 18:35:31 +0000

    Cleaned DataLoading.py and added logs directory to gitignore

--- utils/DataLoading.py
+++ utils/DataLoading.py
@@ -28,7 +28,6 @@ def load_data_into_array(name, scale_factor, is_input=True, normalise_input=True
 
 def data_info(data_arr, dti_param, is_output=False):
     data = data_arr.flatten()
-    out = ""
     if is_output:
         print("Output data from", dti_param)
     else:
@@ -40,22 +39,24 @@ def data_info(data_arr, dti_param, is_output=False):
 
 
 # load numpy arrays
-def load_npz_files(input_list, output_list, dti_param, output_channels):
-    ''' load input and output arrays from lists of npz files '''
+def load_npz_files(input_list, output_list, output_channels, scale_factor=1):
+    """ load input and output arrays from lists of npz files """
+
     # loop through the list and stack arrays
-    global input_arrays, output_arrays
+    input_arrays = np.array([])
+    output_arrays = np.array([])
     for idx, val in enumerate(input_list):
         if idx == 0:
-            input_arrays = load_data_into_array(val)
+            input_arrays = load_data_into_array(val, scale_factor)
         else:
-            temp = load_data_into_array(val)
+            temp = load_data_into_array(val, scale_factor)
             input_arrays = np.concatenate((input_arrays, temp), axis=0)
 
     for idx, val in enumerate(output_list):
         if idx == 0:
-            output_arrays = load_data_into_array(val, is_input=False)
+            output_arrays = load_data_into_array(val, scale_factor, is_input=False)
         else:
-            temp = load_data_into_array(val, is_input=False)
+            temp = load_data_into_array(val, scale_factor, is_input=False)
             output_arrays = np.concatenate((output_arrays, temp), axis=0)
 
     if output_channels == 1:
@@ -64,12 +65,12 @@ def load_npz_files(input_list, output_list, dti_param, output_channels):
     # check there is no nan or inf
     input_arrays = np.nan_to_num(input_arrays)
     output_arrays = np.nan_to_num(output_arrays)
-    assert np.isnan(np.sum(input_arrays)) == False, 'input_array has nans'
-    assert np.isnan(np.sum(output_arrays)) == False, 'output_arrays has nans'
-    assert np.isinf(np.sum(input_arrays)) == False, 'input_array has infs'
-    assert np.isinf(np.sum(output_arrays)) == False, 'output_arrays has infs'
-
+    assert np.isnan(np.sum(input_arrays)) is False, 'input_array has nans'
+    assert np.isnan(np.sum(output_arrays)) is False, 'output_arrays has nans'
+    assert np.isinf(np.sum(input_arrays)) is False, 'input_array has infs'
+    assert np.isinf(np.sum(output_arrays)) is False, 'output_arrays has infs'
     assert input_arrays.shape[0] == output_arrays.shape[0]
+
     return input_arrays, output_arrays
 
 
@@ -80,7 +81,7 @@ def random_crop(img_in, img_out, height, width, input_channels, output_channels)
     cropped_image = tf.image.random_crop(
         stacked_image, size=[height, width, input_channels + output_channels])
 
-    r_img_in = cropped_image[:, :, 0:input_channels]
+    r_img_in = cropped_image[:, :, :input_channels]
     r_img_out = cropped_image[:, :, input_channels:]
     # if OUTPUT_CHANNELS == 1:
     #     r_img_out = tf.expand_dims(r_img_out, axis=2)
@@ -88,14 +89,14 @@ def random_crop(img_in, img_out, height, width, input_channels, output_channels)
     return r_img_in, r_img_out
 
 
-def augment(img_in, img_out, height, width):
+def augment(img_in, img_out, height, width, input_channels, output_channels):
     # Add 20 pixels padding
     add_n = 20
     img_in = tf.image.resize_with_crop_or_pad(
         img_in, height + add_n, width + add_n)
     img_out = tf.image.resize_with_crop_or_pad(
         img_out, height + add_n, width + add_n)
-    img_in, img_out = random_crop(img_in, img_out)
+    img_in, img_out = random_crop(img_in, img_out, height, width, input_channels, output_channels)
 
     return img_in, img_out
 
